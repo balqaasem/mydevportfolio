26935c289d214ad255266bec557811352aa9bcbd
Muhammad-Jibril B.A <jbashir52@gmail.com>
2023-08-12 11:28:47 +0800

    Finish Home Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ module.exports = {
         light: "#f5f5f5",
         primary: "#B63E96", // 240,86,199 - Dark Pink
         primaryDark: "#58E6D9", // 80,230,217 - Electric Blue
+      },
+      animation: {
+        'spin-slow': 'spin 8s linear infinite',
       }
     },
   },
