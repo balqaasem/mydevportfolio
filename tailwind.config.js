cf620de3223b56f8a03c88ce7a8e38c48d0fbeaf
JBA-Khalifa <jbashir52@gmail.com>
2023-08-21 04:38:38 +0800

    update Colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,8 +18,8 @@ module.exports = {
       colors:{
         dark: "#1b1b1b",
         light: "#f5f5f5",
-        primary: "#B63E96", // 240,86,199 - Dark Pink
-        primaryDark: "#58E6D9", // 80,230,217 - Electric Blue
+        primary: "#45f383", // 27,243,258 - Khalifa Green
+        primaryDark: "#51ffe4", // 80,230,217 - Setheum Green
       },
       animation: {
         'spin-slow': 'spin 8s linear infinite',
