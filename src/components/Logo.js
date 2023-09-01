e10c91d5f3159175deec7e6b3a1d13c2b85bdd69
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2023-09-01 09:09:28 +0100

    Update Logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,21 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
 import { motion } from "framer-motion";
+import Image from 'next/image';
+import KhalifaIcon from '../../public/images/profile/KhalifaIcon.png'
 
 const MotionLink = motion(Link);
 
 const Logo = () => {
   return (
     <div className='flex items-center justify-center mt-2'>
-        <MotionLink href="/"
-        className='w-16 h-16 bg-dark text-light flex items-center justify-center
-        rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light'
-        whileHover={{
-            backgroundColor:["#121212", "#84EE5E", "rgba(64,224,208)","rgba(159,239,231)", "rgba(51,179,166)","rgba(54,128,234)"],
-            transition:{duration:1, repeat: Infinity}
-        }}
-        >MBA
-        </MotionLink>
+        <Link href="/"
+        className='w-16 h-16 bg-light dark:bg-dark text-dark dark:text-light flex items-center justify-center
+        rounded-full text-sm font-bold border border-solid border-dark dark:border-light' 
+        >        
+        <Image src={KhalifaIcon} alt={"Khalifa MBA Icon"} className='w-full h-auto'/>
+        100000010
+        </Link>
     </div>
   )
 }
