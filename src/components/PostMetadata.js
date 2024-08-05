dbf9c05d00f42de06ebe184864a42ac4d06f1ce3
Muhammad-Jibril B.A. (Khalifa MBA) <jbashir52@gmail.com>
2024-08-05 10:33:20 +0800

    Create PostMetadata.js

---
+++ src/components/PostMetadata.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {Object} PostMetadata
+ * @property {string} title
+ * @property {string} date
+ * @property {string} subtitle
+ * @property {string} slug
+ */
+
+/**
+ * @type {PostMetadata}
+ */
+
+const PostMetadata = {
+  title: '',
+  date: '',
+  subtitle: '',
+  slug: ''
+};
+
+export { PostMetadata };
